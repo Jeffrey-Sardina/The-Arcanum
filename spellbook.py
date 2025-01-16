883f88fa5f925b067feff4a4597820ab43fcd324
Jeffrey Sardina <jeffrey.sardina@gmail.com>
2025-01-15 21:57:28 -0800

    commenting

--- spellbook.py
+++ spellbook.py
@@ -1,5 +1,6 @@
 '''
-DEFINE FUNCTIONS NEEDED FOR SPELLS
+Background constants and Spell class
+You should not modify this
 '''
 BARDIC_SPELL = 'BARDIC_SPELL'
 FORBIDDEN_SPELL = 'FORBIDDEN_SPELL'
@@ -10,11 +11,20 @@ class Spell:
         self.spell_data = spell_data
         self.spell_type = spell_type
 
+'''
+The Spellbook (mapping of text read from QR codes to actions)
+You should modify this with your own music / image references
+You should not modify anything to do with forbidden spells
+'''
 SPELLBOOK = {
+    # Forbidden spells -- not not modify
     'stop-music': Spell('stop-music', FORBIDDEN_SPELL),
     'stop-illusion': Spell('stop-illusion', FORBIDDEN_SPELL),
     'exit': Spell('exit', FORBIDDEN_SPELL),
 
+    # Illusion spells -- modify at will
     '1': Spell('images/blue.png', ILLUSION_SPELL),
-    '2': Spell('music/bg3.mp3', BARDIC_SPELL)
+
+    # Music spells -- modify at will
+    '2': Spell('music/bg3.mp3', BARDIC_SPELL),
 }
