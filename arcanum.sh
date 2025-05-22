1155d4ef77394b487b5ad8cefc48fb47d4e84ca7
Jeffrey Sardina <jeffrey.sardina@gmail.com>
2025-05-22 12:42:17 -0700

    code to keep computer active

--- arcanum.sh
+++ arcanum.sh
@@ -1,4 +1,5 @@
 #! /bin/bash
 
 cam_id=$1
-python -u arcanum.py $cam_id -db 1> logs/arcanum.cmd.log 2> logs/arcanum.err.log
+python -u arcanum.py $cam_id -db 1> logs/arcanum.cmd.log 2> logs/arcanum.err.log &
+python -m keep_presence
