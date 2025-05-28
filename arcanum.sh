dff7fcb00f6007e5def1342002b04585d4e9b800
Jeffrey Sardina <jeffrey.sardina@gmail.com>
2025-05-28 11:25:31 -0700

    comment

--- arcanum.sh
+++ arcanum.sh
@@ -3,3 +3,5 @@
 cam_id=$1 # if not provided, your default webcam will be used.
 python -u arcanum.py $cam_id 1> logs/arcanum.cmd.log 2> logs/arcanum.err.log &
 python -m keep_presence # https://github.com/carrot69/keep-presence/
+# p=$(ps -ef | grep "python -m keep_presence" | grep -v "grep" | awk '{print $2}')
+
